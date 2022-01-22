15266f43c0d2873af2bcb8fea1db09b212e7ae84
Shibachan1015 <siba1015@cpost.plala.or.jp>
2022-01-22 23:55:06 +0900

    collatz done

---
+++ flag./haskell/collatz-conjecture/.stack-work/dist/x86_64-osx/Cabal-3.2.1.0/build/test/autogen/Paths_collatz_conjecture.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_collatz_conjecture (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [1,2,1,4] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/takashiba/Exercism/flag./haskell/collatz-conjecture/.stack-work/install/x86_64-osx/d71365b97478a0b343f2137470974c551e26ef1aa81bd12e8dfd2885d7664a1e/8.10.7/bin"
+libdir     = "/Users/takashiba/Exercism/flag./haskell/collatz-conjecture/.stack-work/install/x86_64-osx/d71365b97478a0b343f2137470974c551e26ef1aa81bd12e8dfd2885d7664a1e/8.10.7/lib/x86_64-osx-ghc-8.10.7/collatz-conjecture-1.2.1.4-1TyilqrVQqzLrxv5Ilfyj4-test"
+dynlibdir  = "/Users/takashiba/Exercism/flag./haskell/collatz-conjecture/.stack-work/install/x86_64-osx/d71365b97478a0b343f2137470974c551e26ef1aa81bd12e8dfd2885d7664a1e/8.10.7/lib/x86_64-osx-ghc-8.10.7"
+datadir    = "/Users/takashiba/Exercism/flag./haskell/collatz-conjecture/.stack-work/install/x86_64-osx/d71365b97478a0b343f2137470974c551e26ef1aa81bd12e8dfd2885d7664a1e/8.10.7/share/x86_64-osx-ghc-8.10.7/collatz-conjecture-1.2.1.4"
+libexecdir = "/Users/takashiba/Exercism/flag./haskell/collatz-conjecture/.stack-work/install/x86_64-osx/d71365b97478a0b343f2137470974c551e26ef1aa81bd12e8dfd2885d7664a1e/8.10.7/libexec/x86_64-osx-ghc-8.10.7/collatz-conjecture-1.2.1.4"
+sysconfdir = "/Users/takashiba/Exercism/flag./haskell/collatz-conjecture/.stack-work/install/x86_64-osx/d71365b97478a0b343f2137470974c551e26ef1aa81bd12e8dfd2885d7664a1e/8.10.7/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "collatz_conjecture_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "collatz_conjecture_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "collatz_conjecture_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "collatz_conjecture_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "collatz_conjecture_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "collatz_conjecture_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
--- flag./haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ flag./haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,16 @@
 module CollatzConjecture (collatz) where
 
+-- Maybe a　型 Haskell には他言語でnullと呼ばれている値はない。
+-- MaybeにはNothingとJustという２つのコンストラクタがある。
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz xs 
+    |xs <= 0 = Nothing
+    |otherwise = Just (collatz' xs 0)
+    where
+    collatz' :: Integer -> Integer -> Integer
+    collatz' 1 count = count
+    collatz' xs count
+        |even xs = collatz' (xs `div` 2) (count + 1)
+        |otherwise = collatz' (xs * 3 + 1) (count + 1)
+
+
